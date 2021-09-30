e6f68276e74a91c74dd7890d1f11994e9d6523d3
messaouda <messaoudabenchikh@yahoo.fr>
2021-09-30 16:12:45 +0200

    Modifier la declaration des variable

--- Set.php
+++ Set.php
@@ -5,8 +5,8 @@
 
 class Set
 {
-   public int $nbr_pts_joueur1;
-   public int $nbr_pts_joueur2;
+   public int $nbr_pts_joueur1 ;
+   public int $nbr_pts_joueur2 ;
    public int $ecart;
    public int $nbr_sets_j1=0;
    public int $nbr_sets_j2=0;
@@ -14,21 +14,20 @@ class Set
    public int $i;
 
 
-  public function calculSet()
+  public function calculSet($pts_j1, $pts_j2)
 
   {
 
-  $this->nbr_pts_joueur1 ;
-  $this->nbr_pts_joueur2 ;
-   $ecart = abs($this->nbr_pts_joueur1 - $this->nbr_pts_joueur2); 
+ 
+   $ecart = abs($pts_j1 - $pts_j2); 
   // echo "<br>ecart : ".$ecart;
-   if($ecart >= 2 AND ($this->nbr_pts_joueur1 >=11 || $this->nbr_pts_joueur2 >=11) )
+   if($ecart >= 2 AND ($pts_j1 >=11 || $pts_j2 >=11) )
     {
       //echo "je suis la";
     for($i =0; $i <= 3; $i++ ) 
      {
         //echo "ici aussi";
-         if($this->nbr_pts_joueur1 > $this->nbr_pts_joueur2)
+         if($pts_j1 > $pts_j2)
           {
            $this->nbr_sets_j1 = $this->nbr_sets_j1 + 1;
            //return $this->nbr_sets_j1 ;casser la boucle
