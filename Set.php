dc2db15c18df95d3566b13fc198ce63d7cd1237f
messaouda <messaoudabenchikh@yahoo.fr>
2021-10-06 08:57:18 +0200

    créer la fonction qui calcul les points et une pour determiner à qui le set

--- Set.php
+++ Set.php
@@ -1,50 +1,55 @@
+<?php
 
-<?php 
+class Set{
 
-//class set
+public $pointJ1 = 0;
+public $pointJ2 = 0;
 
-class Set
+private $gagant;
+private $est_fini =false;
+private $partie;
+private $numero;
+public function __construct($partie, $numero){
+    $this->partie = $this->partie;    // chaque set appartient à une partie 
+    $this->numero = $this->numero;    // chaque set est numéroté de 1 à 7 
+    $this->ajoutPoint($this->pointJ1, $this->pointJ2);
+
+}
+public function estFini():bool{
+    return $this->est_fini; 
+}
+
+public function ajoutPoint(int $p1, int $p2){
+    $this->pointJ1 = $this->pointJ1 + $p1; 
+    $this->pointJ2 = $this->pointJ2 + $p2; 
+    // Calcul l'écart 
+$ecart = abs( $this->pointJ1 -  $this->pointJ2);
+echo $ecart;
+// Vérification condition pour avoir un set
+if(($this->pointJ1 >= 11 || $this->pointJ2 >= 11 ) && ($this->ecart >= 2)){
+   echo "set fini:"."<br>";
+   echo "point jour1".$this->pointJ1."point joueur2".$this->pointJ2."<br>" ;
+   $this->pointJ1 = 0;
+   $this->pointJ2 = 0;
+   $this->est_fini = true;
+
+}
+}
+public function getGagnant()
 {
-   public int $nbr_pts_joueur1 ;
-   public int $nbr_pts_joueur2 ;
-   public int $ecart;
-   public int $nbr_sets_j1=0;
-   public int $nbr_sets_j2=0;
-  
-   public int $i;
 
+if($this->estFini($this->pointJ1, $this->pointJ2)){
+    if($this->this->pointJ1 > $this->this->pointJ2){
+echo $this->this->pointJ1->nom()."gagnant";
+    }else{
+        echo $this->this->pointJ2->nom()."gagnant";
+    }
 
-  public function calculSet($pts_j1, $pts_j2)
+}
 
-  {
+}
 
- 
-   $ecart = abs($pts_j1 - $pts_j2); 
-  // echo "<br>ecart : ".$ecart;
-   if($ecart >= 2 AND ($pts_j1 >=11 || $pts_j2 >=11) )
-    {
-      //echo "je suis la";
-    for($i =0; $i <= 3; $i++ ) 
-     {
-        //echo "ici aussi";
-         if($pts_j1 > $pts_j2)
-          {
-           $this->nbr_sets_j1 = $this->nbr_sets_j1 + 1;
-           //return $this->nbr_sets_j1 ;casser la boucle
-          }
-           else
-          {
-           $this->nbr_sets_j2 = $this->nbr_sets_j2 + 1;
-         
-          }
-      }
-      return array($this->nbr_sets_j1 , $this->nbr_sets_j2 );
-    }  
-  }    
- }
-  
-
-
+}
 
 
 
