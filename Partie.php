22471fa2326bc438d7aa355fbfe9796c5f6062d0
messaouda <messaoudabenchikh@yahoo.fr>
2021-10-07 15:23:37 +0200

    Ajout fonction createSet getSetCourant getSets

--- Partie.php
+++ Partie.php
@@ -1,50 +1,68 @@
 <?php
 // La class Partie
-class Partie {
-
-    private $nbr_sets;//Le nombre de set.
-
-  /** 
-     * @var Joueur 
-     */ 
-    private $joueur1;
+class Partie 
+{
+    
+    private $set_courant = 0 ;
+    private $est_fini = false; 
+    private $sets = [];//suivi de set
+    private $nb_sets =3;
      /** 
-     * @var Joueur 
+     * @param Joueur $j1      Joueur numéro 1 
+     * @param Joueur $j2      Joueur numéro 2 
+     * @param int    $nb_sets nombre de sets à jouer : 3 
      */ 
-    private $joueur2;
-     /** 
-     * @var Joueur 
-     */ 
-    private $gagnant; 
-    public function __construct(joueur1 $j1, joueur2 $j2, int $nbr_sets ){
+    public function __construct(Joueur $joueur1, Joueur $joueur2, $nb_sets){
 
-        $this->joueur1= $j1;
-        $this->joueur2= $j2;
-        if($nbr_sets===3)
-        {
-           $this->nbr_sets= $nbr_sets;
-        }
-        else {
-            $this->nbr_sets= 3;
-        }
-        $this->startNewSet();
-    } 
+        $this->joueur1 = $joueur1;
+        $this->joueur2 = $joueur2;
+        
+    }
 
     /** 
      * @return Joueur 
      */ 
-    public function joueur1(): joueur{
-        return $this->joueur1;
-    }
-  /** 
+    public function getJoueur1(): Joueur 
+    { 
+        return $this->joueur1; 
+    } 
+  
+    /** 
      * @return Joueur 
      */ 
-    public function joueur2(): Joueur 
+    public function getJoueur2(): Joueur 
     { 
         return $this->joueur2; 
     } 
-public function startNouveauSet():void{
+  
+    /** 
+     * @return bool 
+     */ 
+    public function estFini() :bool
+    { if($this->nb_sets == 3){
+        $this->est_fini = true;
+    }
+        return $this->est_fini; 
+    } 
 
-    
+    public function createSet() 
+    { 
+        $this->sets[] = new Set();// Ajouter un set 
+          
+    } 
+    public function getSetCourant(): false|Set //Renvoi false ou un set 
+    {
+
+        return end($this->sets);
+    }
+
+    public function getSets(): array  //Renvoi un tableau 
+    {
+        return $this->sets;
+    }
+   
 }
-}
+ 
+  
+
+
